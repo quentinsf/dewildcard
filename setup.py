33191b8d6f852597ddebe8f7eec1c57d6353cdf8
Quentin Stafford-Fraser <quentin@pobox.com>
2018-08-30 12:00:25 +0100

    Update setup.py for new PyPI and add a .gitignore

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="dewildcard",
     # packages = ['dewildcard'],
     scripts=['dewildcard'],
-    version="0.2",
+    version="0.2.1",
     description="Expand wildcard imports in Python code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Quentin Stafford-Fraser",
     author_email="quentin@pobox.com",
     url="http://github.com/quentinsf/dewildcard",
     download_url='https://github.com/quentinsf/dewildcard/tarball/0.2',
-    keywords='pylint'
+    keywords='pylint',
+    classifiers=(
+        "Programming Language :: Python :: 2",
+    )
 )
 
